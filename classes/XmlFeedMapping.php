16dd7d562c8bd5609d801768481c6c818205383b
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-06 15:56:41 +0200

    Update XmlFeedMapping.php

--- classes/XmlFeedMapping.php
+++ classes/XmlFeedMapping.php
@@ -1,1 +1,12 @@
+<?php
+class XmlFeedMapping
+{
+    public $xml_field;
+    public $prestashop_field;
 
+    public function __construct($xml_field, $prestashop_field)
+    {
+        $this->xml_field = $xml_field;
+        $this->prestashop_field = $prestashop_field;
+    }
+}
