6912a77bec9e28aa6e3ca45ba6989d97fd8fc5b1
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-06 15:49:18 +0200

    Update AdminXmlFeedManagerController.php

--- controllers/admin/AdminXmlFeedManagerController.php
+++ controllers/admin/AdminXmlFeedManagerController.php
@@ -1,1 +1,41 @@
+<?php
+class AdminXmlFeedManagerController extends ModuleAdminController
+{
+    public function __construct()
+    {
+        $this->bootstrap = true;
+        parent::__construct();
+    }
 
+    public function initContent()
+    {
+        parent::initContent();
+        $this->setTemplate('configure.tpl');
+    }
+
+    public function postProcess()
+    {
+        if (Tools::isSubmit('submitxmlfeedmanager')) {
+            Configuration::updateValue('XMLFEEDMANAGER_SETTING', Tools::getValue('XMLFEEDMANAGER_SETTING'));
+        }
+
+        if (Tools::isSubmit('addXmlField')) {
+            $field = new XmlFeedField();
+            $field->field_name = Tools::getValue('field_name');
+            $field->prestashop_field = Tools::getValue('prestashop_field');
+            $field->save();
+        }
+
+        if (Tools::isSubmit('deleteXmlField')) {
+            $field = new XmlFeedField((int)Tools::getValue('id_field'));
+            $field->delete();
+        }
+
+        if (Tools::isSubmit('scanXmlFeed')) {
+            $handler = new XmlFeedHandler();
+            $mappings = $handler->scanFeed($_FILES['xml_file']['tmp_name']);
+            $this->context->smarty->assign('mappings', $mappings);
+            $this->setTemplate('recommend_mappings.tpl');
+        }
+    }
+}
