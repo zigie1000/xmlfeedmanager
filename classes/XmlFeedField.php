54800b463fe65f31ab153e25f950fb5a2a3955b7
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-06 15:53:24 +0200

    Update XmlFeedField.php

--- classes/XmlFeedField.php
+++ classes/XmlFeedField.php
@@ -1,1 +1,21 @@
+<?php
+class XmlFeedField extends ObjectModel
+{
+    public $id_field;
+    public $field_name;
+    public $prestashop_field;
 
+    public static $definition = array(
+        'table' => 'xmlfeedmanager_fields',
+        'primary' => 'id_field',
+        'fields' => array(
+            'field_name' => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true, 'size' => 255),
+            'prestashop_field' => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true, 'size' => 255),
+        ),
+    );
+
+    public function __construct($id_field = null)
+    {
+        parent::__construct($id_field);
+    }
+}
