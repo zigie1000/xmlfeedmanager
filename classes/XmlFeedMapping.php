38a3cc43c5f962c473aa73f6e7c75ad8e12aa3ae
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-06 15:58:19 +0200

    Update XmlFeedMapping.php

--- classes/XmlFeedMapping.php
+++ classes/XmlFeedMapping.php
@@ -1,12 +1,48 @@
 <?php
-class XmlFeedMapping
+
+class XmlFeedMapping extends ObjectModel
 {
+    public $id_mapping;
     public $xml_field;
     public $prestashop_field;
 
-    public function __construct($xml_field, $prestashop_field)
+    public static $definition = array(
+        'table' => 'xmlfeedmanager_mappings',
+        'primary' => 'id_mapping',
+        'fields' => array(
+            'xml_field' => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true, 'size' => 255),
+            'prestashop_field' => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true, 'size' => 255),
+        ),
+    );
+
+    public function __construct($id_mapping = null)
     {
-        $this->xml_field = $xml_field;
-        $this->prestashop_field = $prestashop_field;
+        parent::__construct($id_mapping);
+    }
+
+    public static function getAllMappings()
+    {
+        $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'xmlfeedmanager_mappings';
+        return Db::getInstance()->executeS($sql);
+    }
+
+    public static function getMappingById($id_mapping)
+    {
+        $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'xmlfeedmanager_mappings WHERE id_mapping = ' . (int)$id_mapping;
+        return Db::getInstance()->getRow($sql);
+    }
+
+    public static function addMapping($xml_field, $prestashop_field)
+    {
+        $mapping = new XmlFeedMapping();
+        $mapping->xml_field = pSQL($xml_field);
+        $mapping->prestashop_field = pSQL($prestashop_field);
+        return $mapping->add();
+    }
+
+    public static function deleteMapping($id_mapping)
+    {
+        $mapping = new XmlFeedMapping($id_mapping);
+        return $mapping->delete();
     }
 }
