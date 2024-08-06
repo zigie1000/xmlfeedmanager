c453423e07d0f40dbfa936bf95d24dd6e42cd740
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-07 01:56:06 +0200

    Update xmlfeedmanager.js

--- views/js/xmlfeedmanager.js
+++ views/js/xmlfeedmanager.js
@@ -2,23 +2,23 @@ $(document).ready(function() {
     $('#add-feed').click(function() {
         var feedGroup = `
             <div class="form-group">
-                <label class="control-label col-lg-3">${$('#feedNameLabel').val()}</label>
+                <label class="control-label col-lg-3">Feed Name</label>
                 <div class="col-lg-9">
                     <input type="text" name="XMLFEEDMANAGER_FEED_NAMES[]" class="form-control">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-lg-3">${$('#feedUrlLabel').val()}</label>
+                <label class="control-label col-lg-3">Feed URL</label>
                 <div class="col-lg-9">
                     <input type="text" name="XMLFEEDMANAGER_FEED_URLS[]" class="form-control">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-lg-3">${$('#feedTypeLabel').val()}</label>
+                <label class="control-label col-lg-3">Feed Type</label>
                 <div class="col-lg-9">
                     <select name="XMLFEEDMANAGER_FEED_TYPES[]" class="form-control">
-                        <option value="full">${$('#fullLabel').val()}</option>
-                        <option value="update">${$('#updateLabel').val()}</option>
+                        <option value="full">Full</option>
+                        <option value="update">Update</option>
                     </select>
                 </div>
             </div>`;
