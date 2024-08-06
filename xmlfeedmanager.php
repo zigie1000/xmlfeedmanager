a12b076848325a53c36ca28276a67bc28082725c
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-06 18:22:06 +0200

    Update xmlfeedmanager.php

--- xmlfeedmanager.php
+++ xmlfeedmanager.php
@@ -1,130 +1,85 @@
-<?php
-if (!defined('_PS_VERSION_')) {
-    exit;
+public function getContent()
+{
+    $output = null;
+
+    if (Tools::isSubmit('submit' . $this->name)) {
+        $feedNames = Tools::getValue('XMLFEEDMANAGER_FEED_NAMES');
+        $feedUrls = Tools::getValue('XMLFEEDMANAGER_FEED_URLS');
+        Db::getInstance()->execute('TRUNCATE TABLE ' . _DB_PREFIX_ . 'xmlfeedmanager_feeds');
+        foreach ($feedNames as $index => $feedName) {
+            if (!empty($feedName) && !empty($feedUrls[$index])) {
+                Db::getInstance()->insert('xmlfeedmanager_feeds', array(
+                    'feed_name' => pSQL($feedName),
+                    'feed_url' => pSQL($feedUrls[$index]),
+                ));
+            }
+        }
+        $output .= $this->displayConfirmation($this->l('Settings updated'));
+    }
+
+    return $output . $this->renderForm();
 }
 
-class XmlFeedManager extends Module
+protected function renderForm()
 {
-    public function __construct()
-    {
-        $this->name = 'xmlfeedmanager';
-        $this->tab = 'administration';
-        $this->version = '1.1.0';
-        $this->author = 'Marco Zagato';
-        $this->author_uri = 'https://dealbrut.com';
-        $this->bootstrap = true;
-        parent::__construct();
+    $feeds = Db::getInstance()->executeS('SELECT * FROM ' . _DB_PREFIX_ . 'xmlfeedmanager_feeds');
 
-        $this->displayName = $this->l('XML Feed Manager');
-        $this->description = $this->l('Manage multiple XML feeds for importing and updating product data without overwriting existing products.');
+    $feedNames = array();
+    $feedUrls = array();
+
+    foreach ($feeds as $feed) {
+        $feedNames[] = $feed['feed_name'];
+        $feedUrls[] = $feed['feed_url'];
     }
 
-    public function install()
-    {
-        return parent::install() &&
-               $this->registerHook('actionAdminControllerSetMedia') &&
-               $this->installDb();
-    }
-
-    public function uninstall()
-    {
-        return parent::uninstall() && $this->uninstallDb();
-    }
-
-    private function installDb()
-    {
-        $sql1 = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."xmlfeedmanager_feeds` (
-            `id_feed` INT(11) NOT NULL AUTO_INCREMENT,
-            `feed_name` VARCHAR(255) NOT NULL,
-            `feed_url` VARCHAR(255) NOT NULL,
-            PRIMARY KEY (`id_feed`)
-        ) ENGINE="._MYSQL_ENGINE_." DEFAULT CHARSET=utf8;";
-
-        return Db::getInstance()->execute($sql1);
-    }
-
-    private function uninstallDb()
-    {
-        $sql1 = "DROP TABLE IF EXISTS `"._DB_PREFIX_."xmlfeedmanager_feeds`;";
-
-        return Db::getInstance()->execute($sql1);
-    }
-
-    public function getContent()
-    {
-        $output = null;
-
-        if (Tools::isSubmit('submit' . $this->name)) {
-            $feedNames = Tools::getValue('XMLFEEDMANAGER_FEED_NAMES');
-            $feedUrls = Tools::getValue('XMLFEEDMANAGER_FEED_URLS');
-            Db::getInstance()->execute('TRUNCATE TABLE ' . _DB_PREFIX_ . 'xmlfeedmanager_feeds');
-            foreach ($feedNames as $index => $feedName) {
-                if (!empty($feedName) && !empty($feedUrls[$index])) {
-                    Db::getInstance()->insert('xmlfeedmanager_feeds', array(
-                        'feed_name' => pSQL($feedName),
-                        'feed_url' => pSQL($feedUrls[$index]),
-                    ));
-                }
-            }
-            $output .= $this->displayConfirmation($this->l('Settings updated'));
-        }
-
-        return $output . $this->renderForm();
-    }
-
-    protected function renderForm()
-    {
-        $feeds = Db::getInstance()->executeS('SELECT * FROM ' . _DB_PREFIX_ . 'xmlfeedmanager_feeds');
-
-        $fields_form = array(
-            'form' => array(
-                'legend' => array(
-                    'title' => $this->l('Settings'),
+    $fields_form = array(
+        'form' => array(
+            'legend' => array(
+                'title' => $this->l('Settings'),
+            ),
+            'input' => array(
+                array(
+                    'type' => 'hidden',
+                    'name' => 'XMLFEEDMANAGER_FEED_NAMES[]',
                 ),
-                'input' => array(
-                    array(
-                        'type' => 'hidden',
-                        'name' => 'XMLFEEDMANAGER_FEED_NAMES[]',
-                    ),
-                    array(
-                        'type' => 'hidden',
-                        'name' => 'XMLFEEDMANAGER_FEED_URLS[]',
-                    ),
+                array(
+                    'type' => 'hidden',
+                    'name' => 'XMLFEEDMANAGER_FEED_URLS[]',
                 ),
-                'submit' => array(
-                    'title' => $this->l('Save'),
-                    'class' => 'btn btn-default pull-right'
-                )
+            ),
+            'submit' => array(
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right'
             )
-        );
+        )
+    );
 
-        $helper = new HelperForm();
-        $helper->module = $this;
-        $helper->name_controller = $this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
-        $helper->default_form_language = (int)Configuration::get('PS_LANG_DEFAULT');
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-        $helper->title = $this->displayName;
-        $helper->submit_action = 'submit' . $this->name;
-        $helper->fields_value = $this->getConfigFieldsValues($feeds);
+    $helper = new HelperForm();
+    $helper->module = $this;
+    $helper->name_controller = $this->name;
+    $helper->token = Tools::getAdminTokenLite('AdminModules');
+    $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
+    $helper->default_form_language = (int)Configuration::get('PS_LANG_DEFAULT');
+    $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
+    $helper->title = $this->displayName;
+    $helper->submit_action = 'submit' . $this->name;
+    $helper->fields_value = $this->getConfigFieldsValues($feeds);
 
-        return $helper->generateForm(array($fields_form));
+    return $helper->generateForm(array($fields_form));
+}
+
+public function getConfigFieldsValues($feeds)
+{
+    $feedNames = array();
+    $feedUrls = array();
+
+    foreach ($feeds as $feed) {
+        $feedNames[] = $feed['feed_name'];
+        $feedUrls[] = $feed['feed_url'];
     }
 
-    public function getConfigFieldsValues($feeds)
-    {
-        $feedNames = array();
-        $feedUrls = array();
-
-        foreach ($feeds as $feed) {
-            $feedNames[] = $feed['feed_name'];
-            $feedUrls[] = $feed['feed_url'];
-        }
-
-        return array(
-            'XMLFEEDMANAGER_FEED_NAMES[]' => $feedNames,
-            'XMLFEEDMANAGER_FEED_URLS[]' => $feedUrls,
-        );
-    }
+    return array(
+        'XMLFEEDMANAGER_FEED_NAMES[]' => $feedNames,
+        'XMLFEEDMANAGER_FEED_URLS[]' => $feedUrls,
+    );
 }
