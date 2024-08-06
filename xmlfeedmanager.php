e3e7f727d5ce0b34644e305cf934b30ce82bdc60
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-06 17:09:13 +0200

    Update xmlfeedmanager.php

--- xmlfeedmanager.php
+++ xmlfeedmanager.php
@@ -9,14 +9,14 @@ class XmlFeedManager extends Module
     {
         $this->name = 'xmlfeedmanager';
         $this->tab = 'administration';
-        $this->version = '1.0.0';
+        $this->version = '1.1.0';
         $this->author = 'Marco Zagato';
         $this->author_uri = 'https://dealbrut.com';
         $this->bootstrap = true;
         parent::__construct();
 
         $this->displayName = $this->l('XML Feed Manager');
-        $this->description = $this->l('Manage XML feeds for importing and exporting product data.');
+        $this->description = $this->l('Manage multiple XML feeds for importing and updating product data without overwriting existing products.');
     }
 
     public function install()
@@ -33,29 +33,21 @@ class XmlFeedManager extends Module
 
     private function installDb()
     {
-        $sql1 = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."xmlfeedmanager_fields` (
-            `id_field` INT(11) NOT NULL AUTO_INCREMENT,
-            `field_name` VARCHAR(255) NOT NULL,
-            `prestashop_field` VARCHAR(255) NOT NULL,
-            PRIMARY KEY (`id_field`)
+        $sql1 = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."xmlfeedmanager_feeds` (
+            `id_feed` INT(11) NOT NULL AUTO_INCREMENT,
+            `feed_name` VARCHAR(255) NOT NULL,
+            `feed_url` VARCHAR(255) NOT NULL,
+            PRIMARY KEY (`id_feed`)
         ) ENGINE="._MYSQL_ENGINE_." DEFAULT CHARSET=utf8;";
 
-        $sql2 = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."xmlfeedmanager_mappings` (
-            `id_mapping` INT(11) NOT NULL AUTO_INCREMENT,
-            `xml_field` VARCHAR(255) NOT NULL,
-            `prestashop_field` VARCHAR(255) NOT NULL,
-            PRIMARY KEY (`id_mapping`)
-        ) ENGINE="._MYSQL_ENGINE_." DEFAULT CHARSET=utf8;";
-
-        return Db::getInstance()->execute($sql1) && Db::getInstance()->execute($sql2);
+        return Db::getInstance()->execute($sql1);
     }
 
     private function uninstallDb()
     {
-        $sql1 = "DROP TABLE IF EXISTS `"._DB_PREFIX_."xmlfeedmanager_fields`;";
-        $sql2 = "DROP TABLE IF EXISTS `"._DB_PREFIX_."xmlfeedmanager_mappings`;";
+        $sql1 = "DROP TABLE IF EXISTS `"._DB_PREFIX_."xmlfeedmanager_feeds`;";
 
-        return Db::getInstance()->execute($sql1) && Db::getInstance()->execute($sql2);
+        return Db::getInstance()->execute($sql1);
     }
 
     public function getContent()
@@ -63,7 +55,17 @@ class XmlFeedManager extends Module
         $output = null;
 
         if (Tools::isSubmit('submit' . $this->name)) {
-            Configuration::updateValue('XMLFEEDMANAGER_XML_FEED_URL', Tools::getValue('XMLFEEDMANAGER_XML_FEED_URL'));
+            $feedNames = Tools::getValue('XMLFEEDMANAGER_FEED_NAMES');
+            $feedUrls = Tools::getValue('XMLFEEDMANAGER_FEED_URLS');
+            Db::getInstance()->execute('TRUNCATE TABLE ' . _DB_PREFIX_ . 'xmlfeedmanager_feeds');
+            foreach ($feedNames as $index => $feedName) {
+                if (!empty($feedName) && !empty($feedUrls[$index])) {
+                    Db::getInstance()->insert('xmlfeedmanager_feeds', array(
+                        'feed_name' => pSQL($feedName),
+                        'feed_url' => pSQL($feedUrls[$index]),
+                    ));
+                }
+            }
             $output .= $this->displayConfirmation($this->l('Settings updated'));
         }
 
@@ -72,6 +74,8 @@ class XmlFeedManager extends Module
 
     protected function renderForm()
     {
+        $feeds = Db::getInstance()->executeS('SELECT * FROM ' . _DB_PREFIX_ . 'xmlfeedmanager_feeds');
+
         $fields_form = array(
             'form' => array(
                 'legend' => array(
@@ -79,11 +83,12 @@ class XmlFeedManager extends Module
                 ),
                 'input' => array(
                     array(
-                        'type' => 'text',
-                        'label' => $this->l('XML Feed URL'),
-                        'name' => 'XMLFEEDMANAGER_XML_FEED_URL',
-                        'size' => 100,
-                        'required' => true,
+                        'type' => 'hidden',
+                        'name' => 'XMLFEEDMANAGER_FEED_NAMES[]',
+                    ),
+                    array(
+                        'type' => 'hidden',
+                        'name' => 'XMLFEEDMANAGER_FEED_URLS[]',
                     ),
                 ),
                 'submit' => array(
@@ -102,15 +107,24 @@ class XmlFeedManager extends Module
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
         $helper->title = $this->displayName;
         $helper->submit_action = 'submit' . $this->name;
-        $helper->fields_value = $this->getConfigFieldsValues();
+        $helper->fields_value = $this->getConfigFieldsValues($feeds);
 
         return $helper->generateForm(array($fields_form));
     }
 
-    public function getConfigFieldsValues()
+    public function getConfigFieldsValues($feeds)
     {
+        $feedNames = array();
+        $feedUrls = array();
+
+        foreach ($feeds as $feed) {
+            $feedNames[] = $feed['feed_name'];
+            $feedUrls[] = $feed['feed_url'];
+        }
+
         return array(
-            'XMLFEEDMANAGER_XML_FEED_URL' => Tools::getValue('XMLFEEDMANAGER_XML_FEED_URL', Configuration::get('XMLFEEDMANAGER_XML_FEED_URL')),
+            'XMLFEEDMANAGER_FEED_NAMES[]' => $feedNames,
+            'XMLFEEDMANAGER_FEED_URLS[]' => $feedUrls,
         );
     }
 }
