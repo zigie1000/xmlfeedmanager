8497c63394f4a019cd9955cc5f9660ec68c065f1
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-07 17:11:12 +0200

    Update AdminXmlFeedManagerController.php

--- controllers/admin/AdminXmlFeedManagerController.php
+++ controllers/admin/AdminXmlFeedManagerController.php
@@ -5,19 +5,34 @@ class AdminXmlFeedManagerController extends ModuleAdminController
     public function __construct()
     {
         $this->bootstrap = true;
+        $this->name = 'xmlfeedmanager';
         parent::__construct();
     }
 
     public function initContent()
     {
-        // Include the CSS file for the module
-        $this->context->controller->addCSS($this->module->getPathUri() . 'views/css/xmlfeedmanager.css');
-        // Include the JavaScript file for the module
-        $this->context->controller->addJS($this->module->getPathUri() . 'views/js/xmlfeedmanager.js');
-        
+        $this->display = 'view';
         parent::initContent();
-        // Set the template for the configuration page
-        $this->setTemplate('configure.tpl');
+        $this->context->smarty->assign(array(
+            'module_name' => $this->module->name,
+            'current' => self::$currentIndex,
+            'token' => $this->token,
+            'XMLFEEDMANAGER_FEED_NAMES' => Configuration::get('XMLFEEDMANAGER_FEED_NAMES'),
+            'XMLFEEDMANAGER_FEED_URLS' => Configuration::get('XMLFEEDMANAGER_FEED_URLS'),
+            'XMLFEEDMANAGER_FEED_TYPES' => Configuration::get('XMLFEEDMANAGER_FEED_TYPES'),
+            'XMLFEEDMANAGER_MARKUP_PERCENTAGE' => Configuration::get('XMLFEEDMANAGER_MARKUP_PERCENTAGE'),
+            'submit_text' => $this->l('Save'),
+        ));
+        $this->setTemplate('xmlfeedmanager.tpl');
+    }
+
+    public function postProcess()
+    {
+        if (Tools::isSubmit('submit_xmlfeedmanager')) {
+            Configuration::updateValue('XMLFEEDMANAGER_FEED_NAMES', Tools::getValue('XMLFEEDMANAGER_FEED_NAMES'));
+            Configuration::updateValue('XMLFEEDMANAGER_FEED_URLS', Tools::getValue('XMLFEEDMANAGER_FEED_URLS'));
+            Configuration::updateValue('XMLFEEDMANAGER_FEED_TYPES', Tools::getValue('XMLFEEDMANAGER_FEED_TYPES'));
+            Configuration::updateValue('XMLFEEDMANAGER_MARKUP_PERCENTAGE', Tools::getValue('XMLFEEDMANAGER_MARKUP_PERCENTAGE'));
+        }
     }
 }
-?>
