16ecf3504a7c5476a4000f8ce09161077cb2e3fc
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-07 13:37:09 +0200

    Update xmlfeedmanager.js

--- views/js/xmlfeedmanager.js
+++ views/js/xmlfeedmanager.js
@@ -4,19 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
     addButton.className = 'btn btn-default';
     addButton.onclick = function () {
         var feedFormGroup = document.getElementById('feed-form-group');
-        var feedCount = feedFormGroup.children.length / 3;
+        var feedCount = feedFormGroup ? feedFormGroup.children.length / 3 : 0;
+
         var newFeedNameDiv = document.createElement('div');
         newFeedNameDiv.className = 'form-group';
         newFeedNameDiv.innerHTML = '<label class="control-label col-lg-3" for="feed_name_' + feedCount + '">Feed Name</label>' +
             '<div class="col-lg-9">' +
             '<input type="text" id="feed_name_' + feedCount + '" name="XMLFEEDMANAGER_FEED_NAMES[]" class="form-control">' +
             '</div>';
+
         var newFeedUrlDiv = document.createElement('div');
         newFeedUrlDiv.className = 'form-group';
         newFeedUrlDiv.innerHTML = '<label class="control-label col-lg-3" for="feed_url_' + feedCount + '">Feed URL</label>' +
             '<div class="col-lg-9">' +
             '<input type="text" id="feed_url_' + feedCount + '" name="XMLFEEDMANAGER_FEED_URLS[]" class="form-control">' +
             '</div>';
+
         var newFeedTypeDiv = document.createElement('div');
         newFeedTypeDiv.className = 'form-group';
         newFeedTypeDiv.innerHTML = '<label class="control-label col-lg-3" for="feed_type_' + feedCount + '">Feed Type</label>' +
@@ -26,9 +29,14 @@ document.addEventListener('DOMContentLoaded', function () {
             '<option value="update">Update</option>' +
             '</select>' +
             '</div>';
+
         feedFormGroup.appendChild(newFeedNameDiv);
         feedFormGroup.appendChild(newFeedUrlDiv);
         feedFormGroup.appendChild(newFeedTypeDiv);
     };
-    document.querySelector('.panel-footer').insertBefore(addButton, document.querySelector('.btn.btn-default.pull-right'));
+
+    var panelFooter = document.querySelector('.panel-footer');
+    if (panelFooter) {
+        panelFooter.insertBefore(addButton, panelFooter.querySelector('.btn.btn-default.pull-right'));
+    }
 });
