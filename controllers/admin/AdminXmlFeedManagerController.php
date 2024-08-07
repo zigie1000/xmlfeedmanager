6e6dd0bf1da85a395d3c9e27f44fd356d5232af2
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-07 17:02:33 +0200

    Update AdminXmlFeedManagerController.php

--- controllers/admin/AdminXmlFeedManagerController.php
+++ controllers/admin/AdminXmlFeedManagerController.php
@@ -10,7 +10,14 @@ class AdminXmlFeedManagerController extends ModuleAdminController
 
     public function initContent()
     {
+        // Include the CSS file for the module
+        $this->context->controller->addCSS($this->module->getPathUri() . 'views/css/xmlfeedmanager.css');
+        // Include the JavaScript file for the module
+        $this->context->controller->addJS($this->module->getPathUri() . 'views/js/xmlfeedmanager.js');
+        
         parent::initContent();
+        // Set the template for the configuration page
         $this->setTemplate('configure.tpl');
     }
 }
+?>
