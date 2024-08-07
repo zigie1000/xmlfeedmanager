0117851f1188f98cf3a488a36c33c27bf96d29ef
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-07 13:56:16 +0200

    Update xmlfeedmanager.js

--- views/js/xmlfeedmanager.js
+++ views/js/xmlfeedmanager.js
@@ -14,4 +14,21 @@ document.addEventListener('DOMContentLoaded', function () {
         var newFeedUrlDiv = document.createElement('div');
         newFeedUrlDiv.className = 'form-group';
         newFeedUrlDiv.innerHTML = '<label class="control-label col-lg-3" for="feed_url_' + feedCount + '">Feed URL</label>' +
-            '<div class="col-lg-
+            '<div class="col-lg-9">' +
+            '<input type="text" id="feed_url_' + feedCount + '" name="XMLFEEDMANAGER_FEED_URLS[]" class="form-control">' +
+            '</div>';
+        var newFeedTypeDiv = document.createElement('div');
+        newFeedTypeDiv.className = 'form-group';
+        newFeedTypeDiv.innerHTML = '<label class="control-label col-lg-3" for="feed_type_' + feedCount + '">Feed Type</label>' +
+            '<div class="col-lg-9">' +
+            '<select id="feed_type_' + feedCount + '" name="XMLFEEDMANAGER_FEED_TYPES[]" class="form-control">' +
+            '<option value="full">Full</option>' +
+            '<option value="update">Update</option>' +
+            '</select>' +
+            '</div>';
+        feedFormGroup.appendChild(newFeedNameDiv);
+        feedFormGroup.appendChild(newFeedUrlDiv);
+        feedFormGroup.appendChild(newFeedTypeDiv);
+    };
+    document.querySelector('.panel-footer').insertBefore(addButton, document.querySelector('.btn.btn-default.pull-right'));
+});
