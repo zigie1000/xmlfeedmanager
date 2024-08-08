9c2baa3b5acce50a8a168d8486648fdc29df1605
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-08 16:49:48 +0200

    Update PrestaShopFeedTypes.php

--- classes/PrestaShopFeedTypes.php
+++ classes/PrestaShopFeedTypes.php
@@ -5,33 +5,10 @@ class PrestaShopFeedTypes
     public static function getTypes()
     {
         return array(
-            'product' => 'Product',
-            'category' => 'Category',
-            'manufacturer' => 'Manufacturer',
-            'supplier' => 'Supplier',
-            'combination' => 'Combination',
-            'attribute' => 'Attribute',
-            'attribute_group' => 'Attribute Group',
-            'feature' => 'Feature',
-            'feature_value' => 'Feature Value',
-            'customer' => 'Customer',
-            'address' => 'Address',
-            'order' => 'Order',
-            'order_detail' => 'Order Detail',
-            'order_state' => 'Order State',
-            'tax_rules' => 'Tax Rules',
-            'zone' => 'Zone',
-            'currency' => 'Currency',
-            'country' => 'Country',
-            'state' => 'State',
-            'warehouse' => 'Warehouse',
-            'stock_movement' => 'Stock Movement',
-            'supply_order' => 'Supply Order',
-            'carrier' => 'Carrier',
-            'shop' => 'Shop',
-            'shop_group' => 'Shop Group',
-            'cms' => 'CMS',
-            'cms_category' => 'CMS Category',
+            array('id' => '1', 'name' => 'Product Feed'),
+            array('id' => '2', 'name' => 'Category Feed'),
+            // Add other feed types as needed
         );
     }
 }
+?>
